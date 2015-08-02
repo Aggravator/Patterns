73129687572048cc32d271361e78122da040fe5c
Aggravator <phortopiaono@mail.ru>
2015-08-02 11:48:48 -0700

    Strategy has added

--- Patterns/main.cpp
+++ Patterns/main.cpp
@@ -1,10 +1,15 @@
+#include "strategy.h"
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
+void pseudoMain1(){
+    Context<int> context;
+    cout <<"Strategy is not selected: " <<context.performStrategy(2, 3) << endl;
+    context.setStrategy(new SpecificStrategy1<int>());
+    cout << "First strategy is selected: " << context.performStrategy(2, 3) << endl;
+    context.setStrategy(new SpecificStrategy2<int>());
+    cout << "Second strategy is selected: " << context.performStrategy(2, 3) << endl;
+}
+int main(int argc, char ** argv){
+    pseudoMain1();
     return 0;
 }
-
---
+++ Patterns/strategy.h
@@ -0,0 +1,42 @@
+#ifndef STRATEGY_H
+#define STRATEGY_H
+#include <memory>
+#include <iostream>
+template <class T>
+class Strategy{
+public:
+    virtual T algorithm(T a, T b)=0;
+    virtual ~Strategy(){
+        cout << "Strategy is deleted!" << endl;
+    }
+};
+template <class T>
+class SpecificStrategy1 :public Strategy<T>{
+    virtual T algorithm(T a, T b){
+        return a + b;
+    }
+};
+template <class T>
+class SpecificStrategy2 :public Strategy<T>{
+    virtual T algorithm(T a, T b){
+        return a - b;
+    }
+};
+template <class T>
+class Context{
+private:
+    std::shared_ptr< Strategy<T> > strategy_;
+public:
+    Context(Strategy<T> *strategy=nullptr){
+        strategy_.reset(strategy);
+    }
+    void setStrategy(Strategy<T> *strategy = nullptr){
+        strategy_.reset(strategy);
+    }
+    T performStrategy(T a, T b){
+        if (!strategy_) return T();
+        return strategy_->algorithm(a, b);
+    }
+};
+#endif // STRATEGY_H
+
